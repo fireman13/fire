986beac2edf44bd55218f5b31efbe37d90db1924
fireman13 <56390520+fireman13@users.noreply.github.com>
2020-07-31 23:35:51 +0530

    Update scrt.js

--- scrt.js
+++ scrt.js
@@ -1,7 +1,8 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
-var ul = document.querySelector("ul");
-var str = " wishes Neeraj a very Happy Birthday"
+var ul = document.querySelector("p");
+var str = " wishes Neeraj a very Happy Birthday";
+var modal = document.getElementById('id01');
 
 function inputLength()
 {
@@ -10,12 +11,15 @@ function inputLength()
 
 function createListElement()
 {
-	var li = document.createElement("li");
+	var li = document.createElement("p");
 	var p = document.createElement("p");
+	var b = document.createElement("button");
 	p.innerHTML = str;
 	li.appendChild(document.createTextNode(input.value));
 	ul.appendChild(li);
 	ul.appendChild(p);
+	b.appendChild(document.createTextNode("Click Here"));
+	ul.appendChild(b);
 	input.value = "";
 }
 
